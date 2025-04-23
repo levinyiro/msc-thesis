042332a096617251af2d8bfd5bf06e99344d1142
leventenyiro <nyiro.levente@gmail.com>
2025-04-23 09:28:08 +0200

    modify app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,8 @@ import { MonitorService } from './services/monitor.service';
 })
 export class AppComponent implements OnInit, AfterViewInit {
   worker?: Worker;
-  monitorWorker?: Worker;
   loggingInterval: any;
   canvas?: OffscreenCanvas;
-  @ViewChild('inputShowLine') inputShowLine!: ElementRef<HTMLInputElement>;
-  mercuryData: any;
-  venusData: any;
-  earthData: any;
-  marsData: any;
-  jupiterData: any;
-  saturnData: any;
-  uranusData: any;
-  neptuneData: any;
 
   cpuUsage: string = '';
   memoryUsage: string = '';
@@ -41,154 +31,68 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.monitorSystemStats();
 
     this.dataService.getMercuryData().subscribe(data => {
-      this.mercuryData = data;
       if (this.worker) {
-        this.planets.push(this.mercuryData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'mercuryData', mercuryData: data });
       }
     });
     
     this.dataService.getVenusData().subscribe(data => {
-      this.venusData = data;
       if (this.worker) {
-        this.planets.push(this.venusData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'venusData', venusData: data });
       }
     });
 
     this.dataService.getEarthData().subscribe(data => {
-      this.earthData = data;
       if (this.worker) {
-        this.planets.push(this.earthData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'earthData', earthData: data });
       }
     });
 
     this.dataService.getMarsData().subscribe(data => {
-      this.marsData = data;
       if (this.worker) {
-        this.planets.push(this.marsData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'marsData', marsData: data });
       }
     });
 
     this.dataService.getJupiterData().subscribe(data => {
-      this.jupiterData = data;
       if (this.worker) {
-        this.planets.push(this.jupiterData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'jupiterData', jupiterData: data });
       }
     });
 
     this.dataService.getSaturnData().subscribe(data => {
-      this.saturnData = data;
       if (this.worker) {
-        this.planets.push(this.saturnData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'saturnData', saturnData: data });
       }
     });
 
     this.dataService.getUranusData().subscribe(data => {
-      this.uranusData = data;
       if (this.worker) {
-        this.planets.push(this.uranusData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'uranusData', uranusData: data });
       }
     });
 
     this.dataService.getNeptuneData().subscribe(data => {
-      this.neptuneData = data;
       if (this.worker) {
-        this.planets.push(this.neptuneData);
+        this.planets.push(data);
         this.worker.postMessage({ type: 'neptuneData', neptuneData: data });
       }
     });
   }
-
-  monitorSystemStats() {
-    if (this.loggingInterval) {
-      clearInterval(this.loggingInterval);
-    }
-  
-    const estimateGpuPower = (renderer: string): number => {
-      const gpuScores: { [key: string]: number } = {
-        'Apple M2': 95,
-        'Apple M1': 85,
-        'Intel': 40,
-        'NVIDIA': 90,
-        'AMD': 80,
-        'ANGLE': 60,
-      };
-  
-      for (const [key, value] of Object.entries(gpuScores)) {
-        if (renderer.includes(key)) return value;
-      }
-      return 50;
-    };
-  
-    let gpuRenderer = 'Unknown GPU';
-    const canvas = document.createElement('canvas');
-    const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-    if (gl) {
-      const debugInfo = (gl as any).getExtension('WEBGL_debug_renderer_info');
-      if (debugInfo) {
-        gpuRenderer = (gl as any).getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
-      }
-    }
-  
-    const gpuScore = estimateGpuPower(gpuRenderer);
-  
-    let lastFrameTime = performance.now();
-    const frameDurations: number[] = [];
-  
-    const measureGPU = () => {
-      const now = performance.now();
-      const duration = now - lastFrameTime;
-      frameDurations.push(duration);
-      if (frameDurations.length > 60) frameDurations.shift();
-  
-      lastFrameTime = now;
-      requestAnimationFrame(measureGPU);
-    };
-    measureGPU();
-  
-    this.loggingInterval = setInterval(() => {
-      let cpuUsage;
-  
-      if (typeof performance !== 'undefined' && (performance as any).memory) {
-        const memoryInfo = (performance as any).memory;
-        const totalJSHeap = memoryInfo.totalJSHeapSize;
-        const usedJSHeap = memoryInfo.usedJSHeapSize;
-        cpuUsage = (totalJSHeap > 0 ? (usedJSHeap / totalJSHeap) * 100 : 0).toFixed(2);
-      }
-  
-      let memoryUsage = 0;
-      if ((performance as any).memory) {
-        const memoryInfo = (performance as any).memory;
-        memoryUsage = parseFloat((memoryInfo.usedJSHeapSize / 1024 / 1024).toFixed(2));
-      }
-  
-      const gpuUsageFromFPS = Math.max(
-        0,
-        Math.min(100, ((33.33 - Number(this.fps)) / 16.67) * 100)
-      );
-  
-      const finalGpuUsage = ((gpuScore + gpuUsageFromFPS) / 2).toFixed(2);
-  
-      this.monitorService.logMetrics(Number(cpuUsage), memoryUsage, Number(this.fps), Number(finalGpuUsage));
-  
-      this.memoryUsage = `${memoryUsage} MB`;
-      this.cpuUsage = `${cpuUsage}%`;
-      this.gpuUsage = `${finalGpuUsage}%`;
-  
-    }, 1000);
-  }  
 
   ngAfterViewInit() {
     this.worker = new Worker(new URL('src/app/_workers/threejs.worker.ts', import.meta.url));
     const htmlCanvas = document.getElementById('canvas') as any;
     htmlCanvas.width = window.innerWidth;
     htmlCanvas.height = window.innerHeight;
+    this.sortPlanetsByDistance();
 
     if (htmlCanvas.transferControlToOffscreen) {
       const offscreen = htmlCanvas.transferControlToOffscreen() as any;
@@ -266,37 +170,39 @@ export class AppComponent implements OnInit, AfterViewInit {
         }
       });
 
-      if (this.mercuryData) {
+      console.log(this.planets);
+      
+      if (this.planets.find(x => x.name === 'Mercury')) {
         this.worker.postMessage({ type: 'mercuryData', mercuryData: this.mercuryData });
       }
 
-      if (this.venusData) {
-        this.worker.postMessage({ type: 'venusData', venusData: this.venusData });
-      }
+      // if (this.venusData) {
+      //   this.worker.postMessage({ type: 'venusData', venusData: this.venusData });
+      // }
 
-      if (this.earthData) {
-        this.worker.postMessage({ type: 'earthData', earthData: this.earthData });
-      }
+      // if (this.earthData) {
+      //   this.worker.postMessage({ type: 'earthData', earthData: this.earthData });
+      // }
 
-      if (this.marsData) {
-        this.worker.postMessage({ type: 'marsData', marsData: this.marsData });
-      }
+      // if (this.marsData) {
+      //   this.worker.postMessage({ type: 'marsData', marsData: this.marsData });
+      // }
 
-      if (this.jupiterData) {
-        this.worker.postMessage({ type: 'jupiterData', jupiterData: this.jupiterData });
-      }
+      // if (this.jupiterData) {
+      //   this.worker.postMessage({ type: 'jupiterData', jupiterData: this.jupiterData });
+      // }
 
-      if (this.saturnData) {
-        this.worker.postMessage({ type: 'saturnData', saturnData: this.saturnData });
-      }
+      // if (this.saturnData) {
+      //   this.worker.postMessage({ type: 'saturnData', saturnData: this.saturnData });
+      // }
 
-      if (this.uranusData) {
-        this.worker.postMessage({ type: 'uranusData', uranusData: this.uranusData });
-      }
+      // if (this.uranusData) {
+      //   this.worker.postMessage({ type: 'uranusData', uranusData: this.uranusData });
+      // }
 
-      if (this.neptuneData) {
-        this.worker.postMessage({ type: 'neptuneData', neptuneData: this.neptuneData });
-      }
+      // if (this.neptuneData) {
+      //   this.worker.postMessage({ type: 'neptuneData', neptuneData: this.neptuneData });
+      // }
 
       this.worker.onmessage = (event) => {
         if (event.data.type === 'fps') {
@@ -306,23 +212,104 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
   }
 
-  exportMetricsToCSV(): void {
-    this.monitorService.exportToCSV();
+  // private functions
+  private monitorSystemStats() {
+    if (this.loggingInterval) {
+      clearInterval(this.loggingInterval);
+    }
+  
+    const estimateGpuPower = (renderer: string): number => {
+      const gpuScores: { [key: string]: number } = {
+        'Apple M2': 95,
+        'Apple M1': 85,
+        'Intel': 40,
+        'NVIDIA': 90,
+        'AMD': 80,
+        'ANGLE': 60,
+      };
+  
+      for (const [key, value] of Object.entries(gpuScores)) {
+        if (renderer.includes(key)) return value;
+      }
+      return 50;
+    };
+  
+    let gpuRenderer = 'Unknown GPU';
+    const canvas = document.createElement('canvas');
+    const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    if (gl) {
+      const debugInfo = (gl as any).getExtension('WEBGL_debug_renderer_info');
+      if (debugInfo) {
+        gpuRenderer = (gl as any).getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+      }
+    }
+  
+    const gpuScore = estimateGpuPower(gpuRenderer);
+  
+    let lastFrameTime = performance.now();
+    const frameDurations: number[] = [];
+  
+    const measureGPU = () => {
+      const now = performance.now();
+      const duration = now - lastFrameTime;
+      frameDurations.push(duration);
+      if (frameDurations.length > 60) frameDurations.shift();
+  
+      lastFrameTime = now;
+      requestAnimationFrame(measureGPU);
+    };
+    measureGPU();
+  
+    this.loggingInterval = setInterval(() => {
+      let cpuUsage;
+  
+      if (typeof performance !== 'undefined' && (performance as any).memory) {
+        const memoryInfo = (performance as any).memory;
+        const totalJSHeap = memoryInfo.totalJSHeapSize;
+        const usedJSHeap = memoryInfo.usedJSHeapSize;
+        cpuUsage = (totalJSHeap > 0 ? (usedJSHeap / totalJSHeap) * 100 : 0).toFixed(2);
+      }
+  
+      let memoryUsage = 0;
+      if ((performance as any).memory) {
+        const memoryInfo = (performance as any).memory;
+        memoryUsage = parseFloat((memoryInfo.usedJSHeapSize / 1024 / 1024).toFixed(2));
+      }
+  
+      const gpuUsageFromFPS = Math.max(
+        0,
+        Math.min(100, ((33.33 - Number(this.fps)) / 16.67) * 100)
+      );
+  
+      const finalGpuUsage = ((gpuScore + gpuUsageFromFPS) / 2).toFixed(2);
+  
+      this.monitorService.logMetrics(Number(cpuUsage), memoryUsage, Number(this.fps), Number(finalGpuUsage));
+  
+      this.memoryUsage = `${memoryUsage} MB`;
+      this.cpuUsage = `${cpuUsage}%`;
+      this.gpuUsage = `${finalGpuUsage}%`;
+  
+    }, 1000);
   }
 
-  sortPlanetsByDistance(): void {
-    // this.planets.sort((a, b) => {
-    //   const aAxis = a.data?.semimajorAxis ?? 0;
-    //   const bAxis = b.data?.semimajorAxis ?? 0;
-    //   return aAxis - bAxis;
-    // });
+  private sortPlanetsByDistance(): void {
+    this.planets.sort((a, b) => {
+      const aAxis = a.semimajorAxis ?? 0;
+      const bAxis = b.semimajorAxis ?? 0;
+      return aAxis - bAxis;
+    });
+  }
+
+  // event handlings
+  exportMetricsToCSV(): void {
+    this.monitorService.exportToCSV();
   }
 
   startAddingPlanet() {
     this.isAddingPlanet = true;
     this.newPlanetData = {
       englishName: `Planet ${Math.floor(Math.random() * 1000)}`,
-      color: this.getRandomColor().toString(),
+      color: Math.floor(Math.random() * 0xffffff).toString(),
       size: 0.2 + Math.random() * 0.5,
       semimajorAxis: 0,
       eccentricity: 0,
@@ -347,9 +334,14 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
     this.sortPlanetsByDistance();
   }
-  
-  private getRandomColor(): number {
-    return Math.floor(Math.random() * 0xffffff);
+
+  followPlanet(planet: any) {
+    if (this.worker) {
+      this.worker.postMessage({
+        type: 'followPlanet',
+        planetName: planet.englishName
+      });
+    }
   }
 
   deletePlanet(planet: any) {
@@ -363,18 +355,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.sortPlanetsByDistance();
   }
 
-  followPlanet(planet: any) {
-    if (this.worker) {
-      this.worker.postMessage({
-        type: 'followPlanet',
-        planetName: planet.englishName
-      });
-    }
-  }
-
-  toggleShowPlanetsList() {
-    this.sortPlanetsByDistance();
-    
+  toggleShowPlanetsList() {    
     this.showPlanetsList = !this.showPlanetsList;
   }
 
